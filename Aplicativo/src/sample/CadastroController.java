cdbb1cec6d952e0b68aab04995ea4c3523f89a17
Renan Novak <36902885+RenanNovak@users.noreply.github.com>
2018-09-20 22:00:50 -0300

    .

--- Aplicativo/src/sample/CadastroController.java
+++ Aplicativo/src/sample/CadastroController.java
@@ -1,4 +1,15 @@
 package sample;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class CadastroController {
+
+    public TextField namelogin;
+    public PasswordField senhalogin;
+    public TextField emaillogin;
+    public Button limparlogin;
+
+
 }
--- Aplicativo/src/sample/HomeController.java
+++ Aplicativo/src/sample/HomeController.java
@@ -1,4 +1,9 @@
 package sample;
 
+import javafx.scene.control.Button;
+
 public class HomeController {
+    public Button homelogin;
+    public Button homecontrole;
+
 }
