e61d33b9d11b13235ba19087e38698deaa3a952d
Renan Novak <36902885+RenanNovak@users.noreply.github.com>
2018-09-25 21:29:23 -0300

    .

--- Aplicativo/src/sample/EditarTarefaController.java
+++ Aplicativo/src/sample/EditarTarefaController.java
@@ -1,5 +1,7 @@
 package sample;
 
+import dao.ConnectionFactory;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -9,6 +11,9 @@ import javafx.scene.control.TextField;
 import javafx.scene.web.HTMLEditor;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class EditarTarefaController {
 
@@ -26,5 +31,30 @@ public class EditarTarefaController {
     public void homeedittarefa() throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("Home.fxml"));
         Main.stage.setScene(new Scene(root, 800, 500));
+    }
+
+    @FXML
+    public void salvaredit (ActionEvent actionEvent) throws SQLException {
+
+        Connection conn = ConnectionFactory.getConnection();
+
+        String ssq = "UPDATE tarefa set (nametarefa, textotarefa) " +
+                        "VALUES ( \'" +
+                        nometarefa.getText() + "\', \'" +
+                        textotarefa.getHtmlText() + "\')";
+
+        PreparedStatement stmt = conn.prepareStatement("UPDATE tarefa set (nametarefa, textotarefa) " +
+                "VALUES ( \'" +
+                nometarefa.getText() + "\', \'" +
+                textotarefa.getHtmlText() + "\')");
+
+        stmt.execute();
+        /*PreparedStatement delete = conn.prepareStatement("delete from tarefa");
+        delete.execut1e();*/
+        //Node b = (Node) actionEvent.getSource();
+        // b.getScene().getWindow().hide();
+
+
+        System.out.println(actionEvent.getSource());
     }
 }
